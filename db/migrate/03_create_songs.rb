439914eb95244ffd0b7db20404a06f7854a93eb7
Yonnick <yonnick00@gmail.com>
2019-05-01 14:23:38 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,4 +1,9 @@
-class Song < ActiveRecord::Base
-  belongs_to :artist
-  belongs_to :genre
+class CreateSongs < ActiveRecord::Migration[4.2]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
+    end
+  end
 end
